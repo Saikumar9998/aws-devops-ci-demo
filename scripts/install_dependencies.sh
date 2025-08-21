1f158cc292986b5c3627ef5b2304240d606344c1
Saikumar9998 <129759658+Saikumar9998@users.noreply.github.com>
2025-08-21 13:46:47 +0530

    install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Install Node.js (from NodeSource)
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# Go to app folder and install npm packages
-cd /home/ubuntu/app
-npm install
+sudo apt update -y
+sudo apt install -y apache2
